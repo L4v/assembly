0132314398ebdc6f1a0f929cd4ab5c1c9244164a
l4v <ivos.jovan@gmail.com>
2018-04-20 18:19:48 +0200

    Changes to be committed: 	modified:   glavni.c 	new file:   maska64 	modified:   testiraj.sh 	new file:   z01a.S

--- FTN/source/200418/glavni.c
+++ FTN/source/200418/glavni.c
@@ -2,26 +2,28 @@
 
 unsigned int RUNPP_REG_ERR = 0;
 
-unsigned int maska(unsigned int n, unsigned int v);
+unsigned long long maska64(unsigned int n, unsigned int v);
 
-void printbin(unsigned int x) {
-    unsigned int m=0x80000000, s=0;
+void printbin64(unsigned long long x) {
+    unsigned long long m=0x8000000000000000ULL;
+    int s=0;
     while(m) {
-        printf("%s%s",m&x ? "1" : "0",++s%4 ? "" : "");
+        printf("%s%s",m&x ? "1" : "0",++s%8 ? "" : " ");
         m >>= 1;
     }
 }
 
 int main() {
-    unsigned int n, v, r=0, g=0;
+    unsigned int n, v, g=0;
+    unsigned long long r;
 
     printf("Unesite N: ");
     scanf("%d",&n);
     printf("Unesite V: ");
     scanf("%d",&v);
     printf("Maska: ");
-    r = maska(n,v);
-    printbin(r);
+    r = maska64(n,v);
+    printbin64(r);
     printf("\n");
 
     if (RUNPP_REG_ERR) return RUNPP_REG_ERR+128;
